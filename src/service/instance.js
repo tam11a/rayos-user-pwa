5fe4784874a315b881dd37bcb5f0ba824dc1a3fa
tam11a <ibrahimsadiktamim@gmail.com>
2022-10-17 05:41:23 +0600

    updated: Service API Link

--- src/service/instance.js
+++ src/service/instance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const rootURL = "http://199.192.20.135:4001/";
+export const rootURL = "http://162.0.211.226:4001/";
 export const baseURL = rootURL + "api";
 
 export const authRootURL = "https://oauth.pndservicebd.com/";
