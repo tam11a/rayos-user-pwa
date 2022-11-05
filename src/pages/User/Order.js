0b4f84e3407b61f22e58aedd7d09fcf07b8432d1
tam11a <ibrahimsadiktamim@gmail.com>
2022-11-05 08:12:16 +0600

    added: order.js

--- src/pages/User/Order.js
+++ src/pages/User/Order.js
@@ -1,6 +1,13 @@
 import React from "react";
 
 const Order = () => {
+  
+  const [params, setParams] = React.useState({
+    method: "all",
+    limit: 10,
+    page: 1,
+    filters: [],
+  });
   return <div>Order</div>;
 };
 
