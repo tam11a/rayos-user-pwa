523dc4c1ee3f089e59eb2a13639518ee123bd78d
tam11a <ibrahimsadiktamim@gmail.com>
2022-11-16 07:35:41 +0600

    request over https

--- src/service/instance.js
+++ src/service/instance.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
-export const rootURL = "http://api.rayosbd.com/";
+export const rootURL = "https://api.rayosbd.com/";
 export const baseURL = rootURL + "api";
-
-export const authRootURL = "https://oauth.pndservicebd.com/";
-export const authURL = authRootURL + "api/auth/";
 
 const instance = axios.create({
   // unauthorized instance
@@ -31,26 +28,6 @@ export const updateInstance = () => {
   );
 };
 
-export const authInstance = axios.create({
-  // authentication instance
-  baseURL: authURL,
-  headers: {
-    "Content-Type": "application/json",
-    accept: "*/*",
-  },
-});
-
-export const updateAuthInstance = () => {
-  authInstance.interceptors.request.use(
-    (req) => {
-      req.headers["Authorization"] = `Bearer ${localStorage.getItem("tkn")}`;
-      return req;
-    },
-    (err) => {
-      return Promise.reject(err);
-    }
-  );
-};
 
 export const getAttachment = (attachmentId) => {
   return baseURL + "/attachments/" + attachmentId;
