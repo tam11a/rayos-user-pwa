10e41686cdb8cdb5567e59b63398c12c1a5dc0d4
Asatter833 <asatter833@gmail.com>
2022-12-27 17:38:36 +0600

    Code Splitted

--- src/routes/AppRoute.js
+++ src/routes/AppRoute.js
@@ -1,20 +1,32 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 
 // pages
-import Home from "../pages/Home";
-import About from "../pages/About";
-import PrivacyPolicy from "../pages/PrivacyPolicy";
-import ReturnPolicy from "../pages/ReturnPolicy";
-import DeliveryPolicy from "../pages/DeliveryPolicy";
-import Search from "../pages/Search";
-import Notification from "../pages/Notification";
-import ProcessOrder from "../pages/ProcessOrder";
-import User from "../pages/User";
-import ProductView from "../pages/ProductView";
-import Order from "../pages/User/Order";
-import OrderInfo from "../pages/User/OrderInfo";
-import Test from "../pages/Test";
+// import Home from "../pages/Home";
+// import About from "../pages/About";
+// import PrivacyPolicy from "../pages/PrivacyPolicy";
+// import ReturnPolicy from "../pages/ReturnPolicy";
+// import DeliveryPolicy from "../pages/DeliveryPolicy";
+// import Search from "../pages/Search";
+// import Notification from "../pages/Notification";
+// import ProcessOrder from "../pages/ProcessOrder";
+// import User from "../pages/User";
+// import ProductView from "../pages/ProductView";
+// import Order from "../pages/User/Order";
+// import OrderInfo from "../pages/User/OrderInfo";
+
+const Home = React.lazy(() => import("../pages/Home"));
+const PrivacyPolicy = React.lazy(() => import("../pages/PrivacyPolicy"));
+const ReturnPolicy = React.lazy(() => import("../pages/ReturnPolicy"));
+const Notification = React.lazy(() => import("../pages/Notification"));
+const DeliveryPolicy = React.lazy(() => import("../pages/DeliveryPolicy"));
+const ProcessOrder = React.lazy(() => import("../pages/ProcessOrder"));
+const ProductView = React.lazy(() => import("../pages/ProductView"));
+const Order = React.lazy(() => import("../pages/User/Order"));
+const OrderInfo = React.lazy(() => import("../pages/User/OrderInfo"));
+const Search = React.lazy(() => import("../pages/Search"));
+const About = React.lazy(() => import("../pages/About"));
+const User = React.lazy(() => import("../pages/User"));
 
 const AppRoute = () => {
   const location = useLocation();
@@ -27,27 +39,28 @@ const AppRoute = () => {
   }, [location]);
   return (
     <>
-      <Routes path="/">
-        <Route index element={<Home />} />
-        <Route path="product/:productId" element={<ProductView />} />
-        <Route path="search" element={<Search />} />
-        <Route path="notification" element={<Notification />} />
-        <Route path="about" element={<About />} />
-        <Route path="return-policy" element={<ReturnPolicy />} />
-        <Route path="privacy-policy" element={<PrivacyPolicy />} />
-        <Route path="delivery-policy" element={<DeliveryPolicy />} />
-        <Route path="user">
-          <Route index element={<User />} />
-          <Route path="order">
-            <Route index element={<Order />} />
-            <Route path=":oid" element={<OrderInfo />} />
+      <Suspense>
+        <Routes path="/">
+          <Route index element={<Home />} />
+          <Route path="product/:productId" element={<ProductView />} />
+          <Route path="search" element={<Search />} />
+          <Route path="notification" element={<Notification />} />
+          <Route path="about" element={<About />} />
+          <Route path="return-policy" element={<ReturnPolicy />} />
+          <Route path="privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="delivery-policy" element={<DeliveryPolicy />} />
+          <Route path="user">
+            <Route index element={<User />} />
+            <Route path="order">
+              <Route index element={<Order />} />
+              <Route path=":oid" element={<OrderInfo />} />
+            </Route>
+            {/* <Route path="wallet" element={<Wallet />} /> */}
           </Route>
-          {/* <Route path="wallet" element={<Wallet />} /> */}
-        </Route>
-        <Route path="process-order" element={<ProcessOrder />} />
-        <Route path="test" element={<Test />} />
-        <Route path="*" element={<>404</>} />
-      </Routes>
+          <Route path="process-order" element={<ProcessOrder />} />
+          <Route path="*" element={<>404</>} />
+        </Routes>
+      </Suspense>
     </>
   );
 };
