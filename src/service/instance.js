05d5b60028ebaacd7afc7598362e528c11a0d3df
tam11a <ibrahimsadiktamim@gmail.com>
2023-01-04 21:47:31 +0600

    migrated: http to https

--- src/service/instance.js
+++ src/service/instance.js
@@ -1,36 +1,35 @@
 import axios from "axios";
 
-export const rootURL = "http://api.rayosbd.com/";
+export const rootURL = "https://api.rayosbd.com/";
 export const baseURL = rootURL + "api";
 
 const instance = axios.create({
-  // unauthorized instance
-  baseURL: baseURL,
-  headers: {
-    "Content-Type": "application/json",
-    accept: "*/*",
-    Authorization: `Bearer ${localStorage.getItem("tkn")}`,
-  },
+	// unauthorized instance
+	baseURL: baseURL,
+	headers: {
+		"Content-Type": "application/json",
+		accept: "*/*",
+		Authorization: `Bearer ${localStorage.getItem("tkn")}`,
+	},
 });
 
 export const updateInstance = () => {
-  instance.interceptors.request.use(
-    (req) => {
-      if (localStorage.getItem("tkn"))
-        req.headers["Authorization"] = `Bearer ${localStorage.getItem("tkn")}`;
-      else req.headers["Authorization"] = undefined;
+	instance.interceptors.request.use(
+		(req) => {
+			if (localStorage.getItem("tkn"))
+				req.headers["Authorization"] = `Bearer ${localStorage.getItem("tkn")}`;
+			else req.headers["Authorization"] = undefined;
 
-      return req;
-    },
-    (err) => {
-      return Promise.reject(err);
-    }
-  );
+			return req;
+		},
+		(err) => {
+			return Promise.reject(err);
+		}
+	);
 };
 
-
 export const getAttachment = (attachmentId) => {
-  return baseURL + "/attachments/" + attachmentId;
+	return baseURL + "/attachments/" + attachmentId;
 };
 
 export default instance;
